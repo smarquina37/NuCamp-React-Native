a81f09c0e2ff30c0b559308ea6e28326e16f78ac
Susana Marquina <smarq86@gmail.com>
2021-02-09 21:31:14 -0800

    Alert

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, Text, StyleSheet, Alert } from 'react-native';
 import {ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
@@ -29,25 +29,47 @@ class Favorites extends Component {
         const { navigate } = this.props.navigation;
         const renderFavoriteItem = ({item}) => {
             return (
-                <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
-                    <View style={styles.deleteView}>
-                        <TouchableOpacity
-                        style={styles.deleteTouchable}
-                        onPress={() => this.props.deleteFavorite(item.id)}
-                        >
-                        <Text style={styles.deleteText}>Delete</Text>
-                        </TouchableOpacity>
-                    </View>
+              <SwipeRow rightOpenValue={-100} style={styles.swipeRow}>
+                <View style={styles.deleteView}>
+                  <TouchableOpacity
+                    style={styles.deleteTouchable}
+                    onPress={() =>
+                      Alert.alert(
+                        "Delete Favorite?",
+                        "Are you sure you wish to delete the favorite campsite " +
+                          item.name +
+                          "?",
+                        [
+                          {
+                            text: "Cancel",
+                            onPress: () =>
+                              console.log(item.name + "Not Deleted"),
+                            style: "cancel"
+                          },
+                          {
+                            text: "OK",
+                            onPress: () => this.props.deleteFavorite(item.id)
+                          }
+                        ],
+                        { cancelable: false }
+                      )
+                    }
+                  >
+                    <Text style={styles.deleteText}>Delete</Text>
+                  </TouchableOpacity>
+                </View>
 
-                    <View>
-                        <ListItem
-                            title={item.name}
-                            subtitle={item.description}
-                            leftAvatar={{source: {uri: baseUrl + item.image}}}
-                            onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
-                        />
-                    </View>
-                </SwipeRow>
+                <View>
+                  <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
+                    onPress={() =>
+                      navigate("CampsiteInfo", { campsiteId: item.id })
+                    }
+                  />
+                </View>
+              </SwipeRow>
             );
         };
 
